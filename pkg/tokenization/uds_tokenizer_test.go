5d34690309665d22aaa08102caaa70cf3a672808
Sage Ahrac <sagiahrak@gmail.com>
2026-03-25 19:43:11 +0200

    unused import
    Signed-off-by: Sage Ahrac <sagiahrak@gmail.com>

--- pkg/tokenization/uds_tokenizer_test.go
+++ pkg/tokenization/uds_tokenizer_test.go
@@ -27,7 +27,6 @@ import (
 	tokenizerpb "github.com/llm-d/llm-d-kv-cache/api/tokenizerpb"
 	. "github.com/llm-d/llm-d-kv-cache/pkg/tokenization"
 	types "github.com/llm-d/llm-d-kv-cache/pkg/tokenization/types"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"google.golang.org/grpc"
